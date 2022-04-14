d812c277ee35ecec8d0d796928f55137c812e539
ginohmk <michotall95@gmail.com>
2022-04-14 15:29:54 +0100

    Method fizzbuzz case return buzz

--- solver.rb
+++ solver.rb
@@ -14,5 +14,6 @@ class Solver
   def fizzbuzz(num)
     return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
     return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
   end
 end
